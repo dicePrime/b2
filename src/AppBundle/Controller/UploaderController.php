0931ce16a95bc0aed02b1079e661ff6493382eb2
NDZIE PATRICK JOEL <BMHB8456@adcm.orangecm>
2016-11-02 19:35:45 +0100

    Upload handled perfectly

--- src/AppBundle/Controller/UploaderController.php
+++ src/AppBundle/Controller/UploaderController.php
@@ -17,6 +17,7 @@ use AppBundle\Modele\UtilisateurDAO;
 use AppBundle\Entity\Utilisateur;
 use AppBundle\Modele\DetailsHebdoModel;
 use AppBundle\Entity\DetailsHebdo;
+use AppBundle\Commons\Tools;
 
 /**
  * Description of Uploader
@@ -28,7 +29,27 @@ class UploaderController extends Controller {
     //put your code here
 
     public function uploadAction() {
-        $ds = DIRECTORY_SEPARATOR;  //1
+        
+        $ds = DIRECTORY_SEPARATOR;  // On récupère le séparateur du système de fichier
+        $request = $this->getRequest(); // On récupère la requête associée
+        $session = $request->getSession(); // On récupère la session
+        
+        /**
+         * La variable de session filesToUpload contient la liste de tous 
+         * les fichiers qui doivent être uploadés.
+         * elle est vidées aussitôt que ces fichiers ont été déplacés
+         * vers leur repertoire définitif
+         */
+        if(NULL == $session->get('filesToUpload'))
+        {
+            $filesToUpload = array();
+        }
+        
+        else
+        {
+            $filesToUpload = $session->get('filesToUpload');
+        }
+        
         
         $sessionFolder = 'uploads'.$ds.'tmp'.$ds.session_id();
         if(!file_exists($sessionFolder))
@@ -46,7 +67,62 @@ class UploaderController extends Controller {
             $targetFile = $targetPath . $_FILES['file']['name'];  //5
 
             move_uploaded_file($tempFile, $targetFile); //6
+            
+            $filesToUpload[] = $targetFile;
         }
+        $session->set('filesToUpload', $filesToUpload);
+        return new \Symfony\Component\HttpFoundation\JsonResponse();
+    }
+    /**
+     * Cette fonction AJAX
+     * permet de supprimer un fichier de la liste des fichiers à
+     * uploader
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function deleteFileAction()
+    {
+        $ds = DIRECTORY_SEPARATOR;  //1
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        
+        
+        
+        $fileName = $request->get('filename');
+        
+        if(NULL == $session->get('filesToUpload'))
+        {
+            $filesToUpload = array();
+        }
+        
+        else
+        {
+            $filesToUpload = $session->get('filesToUpload');
+        }
+        
+        Tools::writeFile("beforeDeleta.txt", $filesToUpload);
+        
+        $sessionFolder = 'uploads'.$ds.'tmp'.$ds.session_id();
+        $targetPath = $sessionFolder.$ds.$fileName;
+        Tools::writeFile('testTarget.txt', $targetPath);
+        if(file_exists($targetPath))
+        {
+            
+            unlink($targetPath);
+     
+            $i = 0;
+            foreach($filesToUpload as $file)
+            {
+                if($file == $targetPath)
+                {
+                    Tools::writeFile('occurence.txt', $file);
+                   $filesToUpload[$i] = NULL; 
+                }
+                $i++;
+            }
+        }
+        
+        $session->set('filesToUpload', $filesToUpload);
+        Tools::writeFile("afterDelete.txt", $filesToUpload);
         return new \Symfony\Component\HttpFoundation\JsonResponse();
     }
 
